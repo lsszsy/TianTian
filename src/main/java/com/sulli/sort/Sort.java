37b39d2b31531cb2f54739e36f2fd43b43859a11
lsszsy <superperfectman@sina.com>
2020-01-03 17:08:39 +0800

    测试git

---
+++ src/main/java/com/sulli/sort/Sort.java
@@ -0,0 +1,36 @@
+package com.sulli.sort;
+/**
+ * 快速排序
+ * @author lss
+ *
+ */
+public class Sort {
+	
+	public static int[] qsort(int[] arr, int start, int end){
+		int pivot = arr[start];
+		int i = start;
+		int j = end;
+		while(i<j){
+			while((i<j)&&(arr[j]>pivot)){
+				j--;
+			}
+			while((i<j)&&(arr[i]<pivot)){
+				i++;
+			}
+			if((arr[i]==arr[j])&&(i<j)){
+				i++;
+			}else{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		if(i-1>start){
+			arr = qsort(arr,start,i-1);
+		}
+		if(j+1<end){
+			arr = qsort(arr,j+1,end);
+		}
+		return arr;
+	}
+}
